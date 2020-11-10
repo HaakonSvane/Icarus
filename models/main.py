785c90ddd5e621e0ef58fb496d9b7a2b4064c353
LeiCheng-no <72392271+LeiCheng-no@users.noreply.github.com>
2020-11-10 14:10:55 +0100

    create models

---
+++ models/main.py
@@ -0,0 +1,149 @@
+import glob
+import os
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+from dataset import get_dataset
+from models import RNN
+from models import TCN
+
+
+class TorchDataset(data.Dataset):
+    def __init__(self, dataset_x, dataset_y):
+        self.dataset_x = dataset_x
+        self.dataset_y = dataset_y
+
+    def __getitem__(self, index):
+        return self.dataset_x[index], self.dataset_y[index]
+
+    def __len__(self):
+        return len(self.dataset_x)
+
+
+# hyper parameters
+slice_len = 100
+BATCH_SIZE = 32
+learning_rate = 0.001
+EPOCH = 200
+
+# LSTM
+HIDDEN_SIZE = 5
+LAYER = 2
+
+# CNN
+KERNEL_SIZE = 7
+LEVEL = 3
+nhid = 1
+channel_seq = [nhid] * LEVEL
+
+
+INPUT_SIZE = 5
+CLASSES = 3
+# model = RNN(INPUT_SIZE, HIDDEN_SIZE, LAYER, CLASSES)
+model = TCN(INPUT_SIZE, CLASSES, channel_seq, KERNEL_SIZE, 0)
+
+
+path = r'./labeled/'
+name_all = glob.glob(os.path.join(path, "*.csv"))
+name_all = name_all[:]
+
+x_train_all = []
+x_test_all = []
+y_train_all = []
+y_test_all = []
+for name in name_all:
+    X, Y = get_dataset(name, slice_len)
+    X_tensor = torch.Tensor(X)
+    Y_tensor = torch.Tensor(Y).to(dtype=torch.int64)
+    x_train, x_test, y_train, y_test = train_test_split(X_tensor, Y_tensor, test_size=0.2, random_state=1)
+    x_train_all.append(x_train)
+    x_test_all.append(x_test)
+    y_train_all.append(y_train)
+    y_test_all.append(y_test)
+
+x_train_all = torch.cat(x_train_all, dim=0)
+x_test_all = torch.cat(x_test_all, dim=0)
+y_train_all = torch.cat(y_train_all, dim=0)
+y_test_all = torch.cat(y_test_all, dim=0)
+print(x_train_all.shape)
+print(x_test_all.shape)
+print(y_train_all.shape)
+print(y_test_all.shape)
+
+
+train_length = x_train_all.size(0) * x_train_all.size(1)
+test_length = x_test_all.size(0) * x_test_all.size(1)
+
+
+dataset = TorchDataset(x_train_all, y_train_all)
+loader = data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+loss_list = []
+accuracy_list = []
+loss_test_list = []
+accuracy_test_list = []
+for epoch in range(EPOCH):
+    accuracy_count = 0
+    loss_epoch = 0
+
+    for batch_num, (batch_x, batch_y) in enumerate(loader):
+        optimizer.zero_grad()
+        outputs = model(batch_x)
+
+        outputs = outputs.view(len(batch_y) * slice_len, -1)
+        batch_y = batch_y.view(len(batch_y) * slice_len)
+        loss = criterion(outputs, batch_y)
+        loss.backward()
+        optimizer.step()
+
+        batch_y_pred = outputs.argmax(dim=1)
+        accuracy_count = accuracy_count + sum(torch.eq(batch_y, batch_y_pred)).item()
+        loss_epoch = loss_epoch + loss
+        accuracy = accuracy_count/train_length
+
+    loss_list.append(loss_epoch.item())
+    accuracy_list.append(accuracy)
+
+    with torch.no_grad():
+        outputs_test = model(x_test_all)
+        outputs_test = outputs_test.view(test_length, -1)
+        y_test_all = y_test_all.view(test_length)
+        loss_test = criterion(outputs_test, y_test_all)
+        y_pred_test = outputs_test.argmax(dim=1)
+        accuracy_test = sum(torch.eq(y_test_all, y_pred_test)).item()/test_length
+        loss_test_list.append(loss_test)
+        accuracy_test_list.append(accuracy_test)
+
+    if epoch % 10 == 0:
+        print('epoch:{:d} \ttrain loss:{:f} \ttrain accuracy:{:f} \ttest loss:{:f} \ttest accuracy:{:f}'.format(epoch, loss_epoch.item(), accuracy, loss_test, accuracy_test))
+        plt.plot(y_test_all[:slice_len])
+        plt.plot(y_pred_test[:slice_len])
+        plt.title(epoch)
+        plt.show()
+
+plt.subplot(221)
+plt.plot(loss_list)
+plt.title('train loss')
+plt.xlabel('epoch')
+
+plt.subplot(222)
+plt.plot(accuracy_list)
+plt.title('train accuracy')
+plt.xlabel('epoch')
+
+plt.subplot(223)
+plt.plot(loss_test_list)
+plt.title('test loss')
+plt.xlabel('epoch')
+
+plt.subplot(224)
+plt.plot(accuracy_test_list)
+plt.title('test accuracy')
+plt.xlabel('epoch')
+plt.show()
---
+++ models/models.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils import weight_norm
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)  # x.size(0)是batch_size
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, (_, _) = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        out = F.softmax(self.fc(out), dim=2)
+        return out
+
+
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class TCN(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
+        super(TCN, self).__init__()
+        self.conv = TemporalConvNet(input_size, num_channels, kernel_size, dropout)
+        self.linear = nn.Linear(num_channels[-1], output_size)
+        self.init_weights()
+
+    def init_weights(self):
+        self.linear.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        y1 = self.conv(x)
+        y1 = y1.permute(0, 2, 1)
+        y2 = self.linear(y1)
+        return y2
