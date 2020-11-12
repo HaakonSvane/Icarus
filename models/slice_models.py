b874e06105b748db9f6db4dc215746a2220e7ee3
LeiCheng-no <72392271+LeiCheng-no@users.noreply.github.com>
2020-11-12 20:44:55 +0100

    Update slice_models.py

--- models/slice_models.py
+++ models/slice_models.py
@@ -1,5 +1,40 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.nn.utils import weight_norm
+
+
+# LSTM
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)  # x.size(0)是batch_size
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, (_, _) = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        out = F.softmax(self.fc(out[:, -1, :]), dim=1)
+        return out
+
+
+# GRU
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(GRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)  # x.size(0)是batch_size
+        out, _ = self.lstm(x, h0)  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        out = F.softmax(self.fc(out[:, -1, :]), dim=1)
+        return out
 
 
 # TCN
